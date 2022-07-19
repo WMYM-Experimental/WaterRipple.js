7028da282651bd7da7178f06fe00a6c61be0e3e1
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2022-07-19 18:31:47 -0500

    improve ripple effect

--- js/script.js
+++ js/script.js
@@ -3,43 +3,66 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.fillStyle = "rgba(0, 0, 0, 0.50)";
 
 let mouse = {
   x: 0,
   y: 0,
 };
 
+let counter = 1;
+
 const ripple = [];
+const colors = ["#FFFFFF", "#2C3E50", "#4CA1AF"];
 
 const getRandomNumber = (min, max) => {
-    return Math.random()*(max -min) + 1
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getRandomColor = (colors) => {
+  return colors[getRandomNumber(0, colors.length - 1)];
+};
 
 class Wave {
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.i = getRandomNumber()
+    this.increment = getRandomNumber(2, 5);
   }
   draw() {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = getRandomColor(colors);
+    ctx.shadowColor = getRandomColor(colors);
+    ctx.shadowBlur = 100;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "#000";
     ctx.stroke();
   }
-  update(){
+  update() {
+    this.radius += this.increment;
+    this.draw();
   }
 }
 
 window.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-  const a = new Wave(mouse.x, mouse.y, 50);
-  ripple.push(a);
-  console.log(a);
-  ripple.forEach((w) => {
-    w.draw();
-  });
+  ripple.push(new Wave(mouse.x, mouse.y, getRandomNumber(20, 50)));
 });
 
+const animate = () => {
+  counter++;
+  ripple.forEach((w) => {
+    w.update();
+    if ((w.radius > 100 || w.radius > 300) && counter % 2 == 0) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ripple.shift();
+      counter = 0;
+    }
+  });
+  requestAnimationFrame(animate);
+  console.log(counter);
+};
+
+animate();
